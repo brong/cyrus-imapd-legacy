605062a786914321832921d3e6596762629fd5af
Ken Murchison <murch@andrew.cmu.edu>
2000-07-19 02:01:02 +0000

    ID log tweak

--- imap/imapd.c
+++ imap/imapd.c
@@ -25,7 +25,7 @@
  *  tech-transfer@andrew.cmu.edu
  */
 
-/* $Id: imapd.c,v 1.198.2.3.2.4 2000/07/19 01:51:39 ken3 Exp $ */
+/* $Id: imapd.c,v 1.198.2.3.2.5 2000/07/19 02:01:02 ken3 Exp $ */
 
 #ifndef __GNUC__
 #define __attribute__(foo)
@@ -1530,7 +1530,7 @@ char *cmd;
  */
 #define MAXIDFAILED	3
 #define MAXIDLOG	5
-#define MAXIDLOGLEN	(11 + MAXIDPAIRS * (MAXIDFIELDLEN + MAXIDVALUELEN + 6))
+#define MAXIDLOGLEN	(MAXIDPAIRS * (MAXIDFIELDLEN + MAXIDVALUELEN + 6))
 #define MAXIDFIELDLEN	30
 #define MAXIDVALUELEN	1024
 #define MAXIDPAIRS	30
@@ -1668,7 +1668,7 @@ void cmd_id(char *tag)
     /* log the client's ID string.
        eventually this should be a callback or something. */
     if (npair && logged_id < MAXIDLOG) {
-	char logbuf[MAXIDLOGLEN] = "client id:";
+	char logbuf[MAXIDLOGLEN + 1] = "";
 	struct strlist *fptr, *vptr;
 
 	for (fptr = fields, vptr = values; fptr;
@@ -1684,7 +1684,7 @@ void cmd_id(char *tag)
 			"\"%s\"", vptr->s);
 	}
 
-	syslog(LOG_INFO, "%s", logbuf);
+	syslog(LOG_INFO, "client id:%s", logbuf);
 
 	logged_id++;
     }
