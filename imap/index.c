8cd77bd63dba49acfb9f1fa3585445a574201ded
Ken Murchison <murch@andrew.cmu.edu>
2006-12-19 19:33:22 +0000

    when SORTing by date, fallback to internaldate if the Date header is invalid

--- imap/index.c
+++ imap/index.c
@@ -41,7 +41,7 @@
  *
  */
 /*
- * $Id: index.c,v 1.219.2.3 2006/12/19 18:58:17 murch Exp $
+ * $Id: index.c,v 1.219.2.4 2006/12/19 19:33:22 murch Exp $
  */
 #include <config.h>
 
@@ -4284,10 +4284,12 @@ static int index_sort_compare(MsgData *md1, MsgData *md2,
 	case SORT_CC:
 	    ret = strcmp(md1->cc, md2->cc);
 	    break;
-	case SORT_DATE:
-	    ret = (md1->date && md2->date) ?
-		numcmp(md1->date, md2->date) : numcmp(md1->msgno, md2->msgno);
+	case SORT_DATE: {
+	    time_t d1 = md1->date ? md1->date : INTERNALDATE(md1->msgno);
+	    time_t d2 = md2->date ? md2->date : INTERNALDATE(md2->msgno);
+	    ret = numcmp(d1, d2);
 	    break;
+	}
 	case SORT_FROM:
 	    ret = strcmp(md1->from, md2->from);
 	    break;
