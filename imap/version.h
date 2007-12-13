af0ba01570ac90afcb0fd7f233bff45cead1c7d2
Ken Murchison <murch@andrew.cmu.edu>
2007-12-13 18:30:57 +0000

    go back to version 2.4.0 since we haven't made a release yet

--- imap/version.h
+++ imap/version.h
@@ -37,10 +37,10 @@
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $Id: version.h,v 1.141.2.7 2007/11/08 20:28:02 murch Exp $
+ * $Id: version.h,v 1.141.2.8 2007/12/13 18:30:57 murch Exp $
  */
 
-#define _CYRUS_VERSION "v2.4.1"
+#define _CYRUS_VERSION "v2.4.0"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
