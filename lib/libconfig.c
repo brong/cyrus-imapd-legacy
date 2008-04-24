fe68e011891181fd9945ad96105949f83fd07b91
Ken Murchison <murch@andrew.cmu.edu>
2008-04-24 16:08:32 +0000

    REALLY fix memory corruption (SEGV) with continuation code reading blank lines in imapd.conf (cyrus@fiddaman.net)

--- lib/libconfig.c
+++ lib/libconfig.c
@@ -39,7 +39,7 @@
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $Id: libconfig.c,v 1.18.2.1 2008/04/23 11:00:13 murch Exp $
+ * $Id: libconfig.c,v 1.18.2.2 2008/04/24 16:08:32 murch Exp $
  */
 
 #include <config.h>
@@ -340,11 +340,11 @@ void config_read_file(const char *filename)
     while (fgets(buf+len, bufsize-len, infile)) {
 	if (buf[len]) {
 	    len = strlen(buf);
-	    if (len && buf[len-1] == '\n') {
+	    if (buf[len-1] == '\n') {
 		/* end of line */
 		buf[--len] = '\0';
 
-		if (buf[len-1] == '\\') {
+		if (len && buf[len-1] == '\\') {
 		    /* line continuation */
 		    len--;
 		    lineno++;
