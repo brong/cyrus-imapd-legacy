4bbb835fc8e1783eabddb43edec7191f7abcb8c4
Bron Gondwana <brong@launde.brong.net>
2010-02-10 17:15:13 +1100

    Tool to build debian package directory

---
+++ mkdebian.pl
@@ -0,0 +1,120 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my $num = `git log --oneline | wc -l`;
+chomp($num);
+
+my $branch = `git branch | grep \\\* | cut -c 3-`;
+chomp($branch);
+
+my $date = `date -R`;
+
+my $basename = "cyrus-$branch";
+my $basedir = $branch eq 'fastmail' ? 'usr/cyrus' : "usr/$basename";
+
+mkdir("debian");
+open(FH, ">debian/changelog");
+print FH <<EOF;
+cyrus-$branch ($num-1) experimental; urgency=low
+
+  * basic package set up
+
+ -- Bron Gondwana <brong\@fastmail.fm>  $date
+EOF
+close(FH);
+
+open(FH, ">debian/control");
+print FH <<EOF;
+Source: $basename
+Section: mail
+Priority: extra
+Maintainer: Bron Gondwana <brong\@fastmail.fm>
+
+Package: $basename
+Architecture: all
+Depends: \${shlibs:Depends}
+Description: Cyrus package for branch $branch at FastMail.FM
+
+Package: $basename-build
+Architecture: all
+Depends: \${shlibs:Depends}
+Description: Cyrus package for branch $branch at FastMail.FM - build files
+EOF
+close(FH);
+
+open(FH, ">debian/copyright");
+print FH "See the upstream files at CMU\n";
+close(FH);
+
+open(FH, ">debian/rules");
+print FH <<EOF;
+#!/usr/bin/make -f
+# debian/rules for alien
+
+# Uncomment this to turn on verbose mode.
+#export DH_VERBOSE=1
+
+# Use v4 compatability mode, so ldconfig gets added to maint scripts.
+export DH_COMPAT=4
+
+PACKAGE=\$(shell dh_listpackages)
+
+build:
+	dh_testdir
+	aclocal -I cmulocal
+	autoheader
+	autoconf
+	./configure --with-auth=unix --without-krb --with-perl=/usr/bin/perl --enable-idled --with-idle=idled --with-extraident=git-$branch-$num --prefix=/$basedir -with-cyrus-prefix=/$basedir --with-com_err=/usr --with-zlib --without-snmp --enable-replication
+	make depend
+	make all CFLAGS="-g -fPIC"
+	make -C sieve test
+	touch build
+
+clean:
+	dh_testdir
+	dh_testroot
+	dh_clean -d
+	rm -f build
+
+binary-indep: build
+
+binary-arch: build
+	dh_testdir
+	dh_testroot
+	dh_clean -k -d
+	dh_installdirs
+
+	dh_installdocs
+	dh_installchangelogs
+
+	make install DESTDIR=\$(CURDIR)/debian/$basename
+	install -o root -m 755 sieve/test debian/$basename/$basedir/bin/sieve-test
+	install -o root -m 755 tools/rehash debian/$basename/$basedir/bin/rehash
+	install -o root -m 755 tools/mkimap debian/$basename/$basedir/bin/mkimap
+	install -o root -m 755 tools/translatesieve debian/$basename/$basedir/bin/translatesieve
+	install -o root -m 755 tools/upgradesieve debian/$basename/$basedir/bin/upgradesieve
+
+	# set up source package
+	mkdir -p debian/$basename-build/usr/src/$basename
+	find . -maxdepth 1 -mindepth 1 -not -name debian -print0 | \\
+		xargs -0 -r -i cp -a {} debian/$basename-build/usr/src/$basename/
+	
+	dh_compress
+	dh_makeshlibs
+	dh_installdeb
+	#-dh_shlibdeps
+	dh_gencontrol
+	dh_md5sums
+	dh_builddeb -- -z3
+
+binary: binary-arch
+EOF
+close(FH);
+
+chmod(0755, "debian/rules");
+
+print "Debian build environment for branch \"$branch\" set up 
+
+  - run dpkg-buildpackage to build\n";
