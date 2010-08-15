06d43a8b5ae15bbe54e4c2c912e9b6c5da17edfa
Bron Gondwana <brong@opera.com>
2010-08-15 20:25:44 +1000

    GUID IMAP COMMANDS
    This patch factors out stuff we used to have in the old MD5UUIDs
    patch, the following FETCH responses in imapd:
    
    FETCH DIGEST.SHA1 => 40 character hex string (message sha1)
    FETCH RFC822.SHA1 => 40 character hex string (message sha1, calculated)
    FETCH RFC822.FILESIZE => size of actual file on disk (via stat or mmap)
    
    It also adds a capability string item: "DIGEST=SHA1"
    
    Totally non-standard of course, but way useful for our replication checking
    scripts.  Embrace and extend 'r' us.
    
    Anyone feel like writing an RFC for fetching the digest of a message via
    IMAP?  If the server calculated it on delivery and cached it then you'd have
    a great way to clean up after a UIDVALIDITY change or other destabilising event
    without having to fetch every message again.

--- imap/imapd.c
+++ imap/imapd.c
@@ -319,6 +319,7 @@ struct capa_struct base_capabilities[] = {
     { "MOVE",                  2 }, /* draft */
     { "SPECIAL-USE",           2 },
     { "CREATE-SPECIAL-USE",    2 },
+    { "DIGEST=SHA1",           2 },
 
 #ifdef HAVE_SSL
     { "URLAUTH",               2 },
@@ -4322,6 +4323,13 @@ badannotation:
 	    else goto badatt;
 	    break;
 
+	case 'D':
+	    if (!strcmp(fetchatt.s, "DIGEST.SHA1")) {
+		fa->fetchitems |= FETCH_GUID;
+	    }
+	    else goto badatt;
+	    break;
+
 	case 'E':
 	    if (!strcmp(fetchatt.s, "ENVELOPE")) {
 		fa->fetchitems |= FETCH_ENVELOPE;
@@ -4355,6 +4363,7 @@ badannotation:
 	    }
 	    else goto badatt;
 	    break;
+
 	case 'R':
 	    if (!strcmp(fetchatt.s, "RFC822")) {
 		fa->fetchitems |= FETCH_RFC822|FETCH_SETSEEN;
@@ -4370,6 +4379,12 @@ badannotation:
 	    }
 	    else if (!strcmp(fetchatt.s, "RFC822.TEXT")) {
 		fa->fetchitems |= FETCH_TEXT|FETCH_SETSEEN;
+	    }
+	    else if (!strcmp(fetchatt.s, "RFC822.SHA1")) {
+		fa->fetchitems |= FETCH_SHA1;
+	    }
+	    else if (!strcmp(fetchatt.s, "RFC822.FILESIZE")) {
+		fa->fetchitems |= FETCH_FILESIZE;
 	    }
 	    else if (!strcmp(fetchatt.s, "RFC822.TEXT.PEEK")) {
 		fa->fetchitems |= FETCH_TEXT;
