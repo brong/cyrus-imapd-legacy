47b5b6a3c6b75cc0bc3a69a9def1570e02ea3cff
Greg Banks <gnb@fastmail.fm>
2010-11-08 17:29:00 +1100

    Add conversations database.
    It's a very simple database with only one type of record, which
    maps a message-id with angle brackets, e.g. "<0001@example.com>",
    to a conversation id and a timestamp.  The conversation id is a
    64b int.
    
    struct conversations_state holds a reference to an open conversations
    database.  There can be more than one of these in existance.  There
    will eventually be a conversations databases per-user, but this code
    doesn't know that and just takes a filename.
    
    conversations_open() opens a database handle and starts a transaction.
    
    conversations_close() aborts the transation if it has not been
        commited, and closes the database handle.
    
    conversations_commit() commits a transaction.
    
    conversations_set() stores a conversation id for a msgid, and sets
        the record's timestamp to the current system time.
    
    conversations_get() retrieves the conversation id given a msgid.
    
    conversations_prune() removes all records older than a given time.

--- imap/global.c
+++ imap/global.c
@@ -103,6 +103,7 @@ struct cyrusdb_backend *config_tlscache_db;
 struct cyrusdb_backend *config_ptscache_db;
 struct cyrusdb_backend *config_statuscache_db;
 struct cyrusdb_backend *config_userdeny_db;
+struct cyrusdb_backend *config_conversations_db;
 
 #define MAX_SESSIONID_SIZE 256
 char session_id_buf[MAX_SESSIONID_SIZE];
@@ -207,6 +208,8 @@ int cyrus_init(const char *alt_config, const char *ident, unsigned flags)
 	    cyrusdb_fromname(config_getstring(IMAPOPT_STATUSCACHE_DB));
 	config_userdeny_db =
 	    cyrusdb_fromname(config_getstring(IMAPOPT_USERDENY_DB));
+	config_conversations_db =
+	    cyrusdb_fromname(config_getstring(IMAPOPT_CONVERSATIONS_DB));
 
 	/* configure libcyrus as needed */
 	libcyrus_config_setstring(CYRUSOPT_CONFIG_DIR, config_dir);
