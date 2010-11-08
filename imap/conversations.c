aa7f460e0bad61d7e93075500bce66dcbb87a16a
Greg Banks <gnb@fastmail.fm>
2010-11-08 17:29:00 +1100

    Add conversations database.
    It's a very simple database with only one type of record, which
    maps a message-id with angle brackets, e.g. "<0001@example.com>",
    to a conversation id and a timestamp.  The conversation id is a
    64b int.
    
    struct conversations_state holds a reference to an open conversations
    database.  There can be more than one of these in existance.  There
    will eventually be a conversations databases per-user, but this code
    doesn't know that and just takes a filename.
    
    conversations_open() opens a database handle and starts a transaction.
    
    conversations_close() aborts the transation if it has not been
        commited, and closes the database handle.
    
    conversations_commit() commits a transaction.
    
    conversations_set() stores a conversation id for a msgid, and sets
        the record's timestamp to the current system time.
    
    conversations_get() retrieves the conversation id given a msgid.
    
    conversations_prune() removes all records older than a given time.

---
+++ imap/conversations.c
@@ -0,0 +1,257 @@
+/* conversations.c -- Routines for dealing with the conversation database
+ *
+ * Copyright (c) 1994-2010 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any legal
+ *    details, please contact
+ *      Carnegie Mellon University
+ *      Center for Technology Transfer and Enterprise Creation
+ *      4615 Forbes Avenue
+ *      Suite 302
+ *      Pittsburgh, PA  15213
+ *      (412) 268-7393, fax: (412) 268-7395
+ *      innovation@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include "acl.h"
+#include "annotate.h"
+#include "append.h"
+#include "assert.h"
+#include "charset.h"
+#include "exitcodes.h"
+#include "hash.h"
+#include "imap_err.h"
+#include "global.h"
+#include "imapd.h"
+#include "lsort.h"
+#include "mailbox.h"
+#include "map.h"
+#include "message.h"
+#include "parseaddr.h"
+#include "search_engines.h"
+#include "seen.h"
+#include "statuscache.h"
+#include "strhash.h"
+#include "stristr.h"
+#include "util.h"
+#include "xmalloc.h"
+#include "xstrlcpy.h"
+#include "xstrlcat.h"
+
+#include "conversations.h"
+
+
+/*
+ * In-database format of conversations db record message-id
+ * record, which is keyed on a message-id (including the <>
+ * brackets), e.g. "<123.abc@example.com>".
+ *
+ * Note that the structure is stored in network order for
+ * portability between architecture.  Note also, the structure
+ * is carefully defined to avoid implicit padding which might
+ * make it's binary format non-portable.
+ *
+ * Also note that we don't use bit64, as this would actually
+ * increase sizeof(mrec) with an invisible 32b pad.
+ */
+struct conversations_mrec {
+    bit32   idhi;	/* low 32 bits of conversation id */
+    bit32   idlo;	/* high 32 bits of conversation id */
+    bit32   stamp;	/* time_t insertion timestamp */
+};
+
+
+#define DB config_conversations_db
+
+int conversations_open(struct conversations_state *state, const char *fname)
+{
+    int r;
+
+    if (!fname)
+	return IMAP_MAILBOX_BADNAME;
+    memset(state, 0, sizeof(*state));
+
+    r = DB->open(fname, CYRUSDB_CREATE, &state->db);
+    if (r || state->db == NULL)
+	return IMAP_IOERROR;
+
+    return 0;
+}
+
+int conversations_close(struct conversations_state *state)
+{
+    if (state->txn != NULL) {
+	DB->abort(state->db, state->txn);
+	state->txn = NULL;
+    }
+    if (state->db != NULL) {
+	DB->close(state->db);
+	state->db = NULL;
+    }
+    return 0;
+}
+
+int conversations_commit(struct conversations_state *state)
+{
+    int r = 0;
+
+    if (state->db != NULL && state->txn != NULL) {
+	r = DB->commit(state->db, state->txn);
+	state->txn = NULL;
+    }
+
+    return r;
+}
+
+static int check_msgid(const char *msgid, int len, int *lenp)
+{
+    if (msgid == NULL)
+	return IMAP_INVALID_IDENTIFIER;
+
+    if (!len)
+	len = strlen(msgid);
+
+    if (msgid[0] != '<' || msgid[len-1] != '>' || strchr(msgid, '@') == NULL)
+	return IMAP_INVALID_IDENTIFIER;
+
+    if (lenp)
+	*lenp = len;
+    return 0;
+}
+
+int conversations_set(struct conversations_state *state,
+		      const char *msgid,
+		      conversation_id_t cid)
+{
+    int keylen;
+    struct conversations_mrec mrec;
+    int r;
+
+    if (state->db == NULL)
+	return IMAP_IOERROR;
+    r = check_msgid(msgid, 0, &keylen);
+    if (r)
+	return r;
+
+    memset(&mrec, 0, sizeof(mrec));
+    align_htonll(&mrec.idhi, cid);
+    mrec.stamp = htonl(time(NULL));
+
+    r = DB->store(state->db,
+		  msgid, keylen,
+		  (const char *)&mrec, sizeof(mrec),
+		  &state->txn);
+    if (r)
+	return IMAP_IOERROR;
+    return 0;
+}
+
+int conversations_get(struct conversations_state *state,
+		      const char *msgid,
+		      conversation_id_t *cidp)
+{
+    int keylen;
+    struct conversations_mrec *mrec = NULL;
+    int datalen = 0;
+    int r;
+
+    if (state->db == NULL)
+	return IMAP_IOERROR;
+    r = check_msgid(msgid, 0, &keylen);
+    if (r)
+	return r;
+
+    r = DB->fetch(state->db,
+		  msgid, keylen,
+		  (const char **)&mrec, &datalen,
+		  &state->txn);
+    if (r == CYRUSDB_NOTFOUND) {
+	*cidp = NULLCONVERSATION;
+	return 0;
+    }
+    if (r || mrec == NULL || datalen != sizeof(*mrec))
+	return IMAP_IOERROR;
+
+    *cidp = align_ntohll(&mrec->idhi);
+    return 0;
+}
+
+struct prune_rock {
+    struct conversations_state *state;
+    time_t thresh;
+};
+
+static int prunecb(void *rock,
+		   const char *key, int keylen,
+		   const char *data, int datalen)
+{
+    struct prune_rock *prock = (struct prune_rock *)rock;
+    const struct conversations_mrec *mrec = (struct conversations_mrec *)data;
+    time_t stamp;
+    int r;
+
+    r = check_msgid(key, keylen, NULL);
+    if (r)
+	return r;
+    if (datalen != sizeof(*mrec))
+	return IMAP_IOERROR;
+
+    stamp = ntohl(mrec->stamp);
+    if (stamp >= prock->thresh)
+	return 0;
+
+    return DB->delete(prock->state->db,
+		      key, keylen,
+		      &prock->state->txn,
+		      /*force*/1);
+}
+
+int conversations_prune(struct conversations_state *state, time_t thresh)
+{
+    struct prune_rock rock = { state, thresh };
+
+    DB->foreach(state->db, "<", 1, NULL, prunecb, &rock, &state->txn);
+
+    return 0;
+}
+
+#undef DB
