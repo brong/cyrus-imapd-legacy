ba62905c5bc193183444909ab6ae5eca4d0448e5
Greg Banks <gnb@fastmail.fm>
2011-02-07 14:21:31 +1100

    Add mailbox actions file.
    This is a file where actions can be posted, that are then later executed
    by the first process to open the mailbox with an exclusive lock.  A
    fcntl lock is used to provide exclusion between writers to file file,
    and to detect when a writer has lost a race with an opener (who renames
    the actions file before excuting the actions therein).  Actions are
    simple lines of text.
    
    Currently only a single action is implemented, the NOP action which just
    increments a count.  This is for testing purposes.
    
    The purpose of the mechanism is to allow delayed renaming of CIDs in
    mailbox index files.

--- imap/mailbox.h
+++ imap/mailbox.h
@@ -77,13 +77,17 @@
 #define FNAME_CACHE "/cyrus.cache"
 #define FNAME_SQUAT "/cyrus.squat"
 #define FNAME_EXPUNGE "/cyrus.expunge"
+#define FNAME_ACTIONS "/cyrus.actions"
+#define FNAME_ACTIONSX "/cyrus.actionsx"
 
 enum meta_filename {
   META_HEADER = 1,
   META_INDEX,
   META_CACHE,
   META_SQUAT,
-  META_EXPUNGE
+  META_EXPUNGE,
+  META_ACTIONS,
+  META_ACTIONSX
 };
 
 #define MAILBOX_FNAME_LEN 256
@@ -523,5 +527,11 @@ extern int mailbox_repack_add(struct mailbox_repack *repack,
 			      struct index_record *record);
 extern void mailbox_repack_abort(struct mailbox_repack **repackptr);
 extern int mailbox_repack_commit(struct mailbox_repack **repackptr);
+
+/* used for testing */
+extern int mailbox_post_nop_action(const char *name, unsigned int tag);
+extern unsigned int mailbox_nop_action_count, mailbox_nop_action_tag;
+extern unsigned int mailbox_nop_action_count;
+extern unsigned int mailbox_nop_action_tag;
 
 #endif /* INCLUDED_MAILBOX_H */
