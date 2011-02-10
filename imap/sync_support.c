c3de1f568033fb982e12719a8085a091a03efe62
Greg Banks <gnb@fastmail.fm>
2011-02-10 15:38:38 +1100

    Handle CID clashing in APPEND, COPY, LMTP
    When a message is added which would belong to two or more conversations,
    the conversations are merged by choosing the maximum of the CIDs
    involved as the winner and renaming all the losing CIDs to the winner in
    the current transaction on the conversations db.  Mailbox actions are
    also posted to cause CID renames in index files the next time the
    mailbox is opened with LOCK_EXCLUSIVE.  To provide the data structures
    to do those notifications, the cid -> folders mapping in the
    conversations db is also updated on every message insert.

--- imap/sync_support.c
+++ imap/sync_support.c
@@ -1599,7 +1599,8 @@ int sync_append_copyfile(struct mailbox *mailbox,
 	free(path);
 	if (!r) {
 	    record->cid = cid;	/* use the CID given us */
-	    r = message_update_conversations(&conversations, record, body);
+	    r = message_update_conversations(&conversations, record,
+					     body, mailbox->name);
 	    if (!r)
 		r = conversations_commit(&conversations);
 	    r = conversations_close(&conversations);
