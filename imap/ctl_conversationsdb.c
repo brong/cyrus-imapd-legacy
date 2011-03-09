87af614fb7cbe4282a7697205e27fbe489319996
Bron Gondwana <brong@opera.com>
2011-03-09 14:20:07 +0100

    ctl_conversationsdb: zero db and build options

--- imap/ctl_conversationsdb.c
+++ imap/ctl_conversationsdb.c
@@ -54,8 +54,11 @@
 #include "assert.h"
 #include "exitcodes.h"
 #include "global.h"
+#include "imap_err.h"
 #include "index.h"
 #include "conversations.h"
+#include "mboxlist.h"
+#include "message.h"
 #include "sysexits.h"
 #include "util.h"
 #include "xmalloc.h"
@@ -138,6 +141,146 @@ out:
     return r;
 }
 
+static int zero_cid_cb(const char *mboxname,
+		       int matchlen __attribute__((unused)),
+		       int maycreate __attribute__((unused)),
+		       void *rock __attribute__((unused)))
+{
+    struct mailbox *mailbox = NULL;
+    struct index_record record;
+    int r;
+    uint32_t recno;
+
+    r = mailbox_open_iwl(mboxname, &mailbox);
+    if (r) return r;
+
+    for (recno = 1; recno <= mailbox->i.num_records; recno++) {
+	r = mailbox_read_index_record(mailbox, recno, &record);
+	if (r) goto done;
+
+	/* already zero, fine */
+	if (record.cid == NULLCONVERSATION)
+	    continue;
+    
+	record.cid = NULLCONVERSATION;
+	r = mailbox_rewrite_index_record(mailbox, &record);
+	if (r) goto done;
+    }
+
+ done:
+    mailbox_close(&mailbox);
+    return r;
+}
+
+static int do_zero(const char *inboxname, const char *fname)
+{
+    char buf[MAX_MAILBOX_NAME];
+    int r;
+
+    /* XXX: truncate?  What about corruption */
+    unlink(fname);
+
+    r = zero_cid_cb(inboxname, 0, 0, NULL);
+    if (r) return r;
+
+    snprintf(buf, sizeof(buf), "%s.*", inboxname);
+    r = mboxlist_findall(NULL, buf, 1, NULL,
+			 NULL, zero_cid_cb, NULL);
+
+    return r;
+}
+
+static int build_cid_cb(const char *mboxname,
+		        int matchlen __attribute__((unused)),
+		        int maycreate __attribute__((unused)),
+		        void *rock)
+{
+    struct conversations_state *state = (struct conversations_state *)rock;
+    struct mailbox *mailbox = NULL;
+    struct body *body = NULL;
+    const char *fname = NULL;
+    struct index_record record;
+    uint32_t recno;
+    int r;
+    FILE *f;
+
+    r = mailbox_open_iwl(mboxname, &mailbox);
+    if (r) return r;
+
+    for (recno = 1; recno <= mailbox->i.num_records; recno++) {
+	r = mailbox_read_index_record(mailbox, recno, &record);
+	if (r) goto done;
+
+	/* already assigned, fine */
+	if (record.cid != NULLCONVERSATION)
+	    continue;
+
+	/* we don't care about expunged messages */
+	if (record.system_flags & FLAG_EXPUNGED)
+	    continue;
+    
+	/* parse the file - XXX: should abstract this bit */
+	fname = mailbox_message_fname(mailbox, record.uid);
+	f = fopen(fname, "r");
+	if (!f) {
+	    r = IMAP_IOERROR;
+	    goto done;
+	}
+	r = message_parse_file(f, NULL, NULL, &body);
+	fclose(f);
+	if (r) goto done;
+
+	r = message_update_conversations(state, &record,
+					 body, mailbox->name);
+	if (r) goto done;
+
+	message_free_body(body);
+	body = NULL;
+
+	r = mailbox_rewrite_index_record(mailbox, &record);
+	if (r) goto done;
+    }
+
+ done:
+    if (body) message_free_body(body);
+    mailbox_close(&mailbox);
+    return r;
+}
+
+static int do_build(const char *inboxname, const char *fname)
+{
+    struct conversations_state state;
+    char buf[MAX_MAILBOX_NAME];
+    int r;
+
+    r = conversations_open(&state, fname);
+    if (r) {
+	/* TODO: wouldn't it be nice if we could translate this
+	 * error code into somethine useful for humans? */
+	fprintf(stderr, "Failed to open conversations database %s: %s\n",
+		fname, error_message(r));
+	return -1;
+    }
+
+    r = build_cid_cb(inboxname, 0, 0, &state);
+    if (r) goto out;
+
+    snprintf(buf, sizeof(buf), "%s.*", inboxname);
+    r = mboxlist_findall(NULL, buf, 1, NULL,
+			 NULL, build_cid_cb, &state);
+    if (r) goto out; 
+
+    r = conversations_commit(&state);
+    if (r) {
+	fprintf(stderr, "Failed to commit conversations database %s: %s\n",
+		fname, error_message(r));
+    }
+
+out:
+    conversations_close(&state);
+    return r;
+}
+
 static int usage(const char *name)
     __attribute__((noreturn));
 
@@ -148,24 +291,39 @@ int main(int argc, char **argv)
     const char *userid = NULL;
     const char *inboxname = NULL;
     char *fname;
-    enum { UNKNOWN, DUMP, UNDUMP } mode = UNKNOWN;
+    int r = 0;
+    enum { UNKNOWN, DUMP, UNDUMP, ZERO, BUILD } mode = UNKNOWN;
 
     if ((geteuid()) == 0 && (become_cyrus() != 0)) {
 	fatal("must run as the Cyrus user", EC_USAGE);
     }
 
-    while ((c = getopt(argc, argv, "duvC:")) != EOF) {
+    while ((c = getopt(argc, argv, "duzbvC:")) != EOF) {
 	switch (c) {
 	case 'd':
 	    if (mode != UNKNOWN)
 		usage(argv[0]);
 	    mode = DUMP;
 	    break;
+
 	case 'u':
 	    if (mode != UNKNOWN)
 		usage(argv[0]);
 	    mode = UNDUMP;
 	    break;
+
+	case 'z':
+	    if (mode != UNKNOWN)
+		usage(argv[0]);
+	    mode = ZERO;
+	    break;
+
+	case 'b':
+	    if (mode != UNKNOWN)
+		usage(argv[0]);
+	    mode = BUILD;
+	    break;
+
 	case 'v':
 	    verbose++;
 	    break;
@@ -190,6 +348,10 @@ int main(int argc, char **argv)
 
     cyrus_init(alt_config, "ctl_conversationsdb", 0);
 
+    mboxlist_init(0);
+    mboxlist_open(NULL);
+
+
     inboxname = mboxname_user_inbox(userid);
     if (inboxname == NULL) {
 	fprintf(stderr, "Invalid userid %s", userid);
@@ -208,33 +370,49 @@ int main(int argc, char **argv)
     {
     case DUMP:
 	if (do_dump(fname))
-	    exit(EC_NOINPUT);
+	    r = EC_NOINPUT;
 	break;
+
     case UNDUMP:
 	if (do_undump(fname))
-	    exit(EC_NOINPUT);
+	    r = EC_NOINPUT;
 	break;
-    case UNKNOWN:   /* oh shut up, gcc */
+
+    case ZERO:
+	if (do_zero(inboxname, fname))
+	    r = EC_NOINPUT;
 	break;
+
+    case BUILD:
+	if (do_build(inboxname, fname))
+	    r = EC_NOINPUT;
+	break;
+
+    case UNKNOWN:
+	fatal("UNKNOWN MODE", EC_SOFTWARE);
     }
+    
+    mboxlist_close();
+    mboxlist_done();
 
     cyrus_done();
     free(fname);
     fname = NULL;
 
-    return 0;
+    return r;
 }
 
 static int usage(const char *name)
 {
-    fprintf(stderr, "usage: %s [options] -d mboxname > dump.dat\n", name);
-    fprintf(stderr, "       %s [options] -u mboxname < dump.dat\n", name);
+    fprintf(stderr, "usage: %s [options] [-u|-d|-z|-f] username\n", name);
+    fprintf(stderr, "\n");
     fprintf(stderr, "options are:\n");
     fprintf(stderr, "    -v             be more verbose\n");
     fprintf(stderr, "    -C altconfig   use altconfig instead of imapd.conf\n");
     fprintf(stderr, "    -u             undump the conversations database from stdin\n");
     fprintf(stderr, "    -d             dump the conversations database to stdout\n");
-
+    fprintf(stderr, "    -z             zero the conversations DB (make all NULLs)\n");
+    fprintf(stderr, "    -b             build conversations entries for any NULL records\n");
 
     exit(EC_USAGE);
 }
