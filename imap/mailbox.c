8dd6e4cbe6cbfbdd413b32bd241804c40a7e4794
Bron Gondwana <brong@opera.com>
2011-03-14 14:57:57 +0100

    use per-user meta files to serialize uidvalidity and highestmodseq
    This covers two things:
    
    1) HIGHESTMODSEQ - the spec leaves it open to have a single per-user
       modseq counter which gets incremented for ANY change to any user
       mailbox.  This means that you only need to track a single
       HIGHESTMODSEQ across all folders.
    
    2) UIDVALIDITY - by having the UIDVALIDITY always increase between folder
       creations, and always be increased by any rename as well, you can
       similarly track just a single value (the highest UIDVALIDITY across
       all folders) and immediately know if a folder is "new".  This also
       avoids the risk here:
    
       http://www.mail-archive.com/imap@u.washington.edu/msg00954.html
    
       even if the clock gets reset back in time.

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -979,7 +979,8 @@ void mailbox_modseq_dirty(struct mailbox *mailbox)
     if (mailbox->modseq_dirty)
 	return;
 
-    mailbox->i.highestmodseq++;
+    mailbox->i.highestmodseq = mboxname_nextmodseq(mailbox->name,
+			       mailbox->i.highestmodseq);
     mailbox->last_updated = time(0);
     mailbox->modseq_dirty = 1;
     mailbox_index_dirty(mailbox);
@@ -1609,7 +1610,8 @@ restart:
 
     /* fix up 2.4.0 bug breakage */
     if (mailbox->i.uidvalidity == 0) {
-	mailbox->i.uidvalidity = time(0);
+	mailbox->i.uidvalidity = mboxname_nextuidvalidity(mailbox->name,
+							  time(0));
 	if (locktype == LOCK_EXCLUSIVE) {
 	    mailbox_index_dirty(mailbox);
 	    mailbox_commit(mailbox);
@@ -1617,7 +1619,7 @@ restart:
 	}
     }
     if (mailbox->i.highestmodseq == 0) {
-	mailbox->i.highestmodseq = 1;
+	mailbox->i.highestmodseq = mboxname_nextmodseq(mailbox->name, 0);
 	if (locktype == LOCK_EXCLUSIVE) {
 	    mailbox_index_dirty(mailbox);
 	    mailbox_commit(mailbox);
@@ -2718,7 +2720,7 @@ int mailbox_create(const char *name,
 
     /* ensure a UIDVALIDITY is set */
     if (!uidvalidity)
-	uidvalidity = time(0);
+	uidvalidity = mboxname_nextuidvalidity(name, time(0));
     /* init non-zero fields */
     mailbox_index_dirty(mailbox);
     mailbox->i.minor_version = MAILBOX_MINOR_VERSION;
@@ -2726,7 +2728,7 @@ int mailbox_create(const char *name,
     mailbox->i.record_size = INDEX_RECORD_SIZE;
     mailbox->i.uidvalidity = uidvalidity;
     mailbox->i.options = options;
-    mailbox->i.highestmodseq = 1;
+    mailbox->i.highestmodseq = mboxname_nextmodseq(mailbox->name, 0);
 
     /* initialise header size field so appends calculate the
      * correct map size */
@@ -4082,7 +4084,8 @@ int mailbox_reconstruct(const char *name, int flags)
     /* fix up 2.4.0 bug breakage */
     if (mailbox->i.uidvalidity == 0) {
 	if (make_changes) {
-	    mailbox->i.uidvalidity = time(0);
+	    mailbox->i.uidvalidity = mboxname_nextuidvalidity(mailbox->name,
+							      time(0));
 	    mailbox_index_dirty(mailbox);
 	}
 	syslog(LOG_ERR, "%s: zero uidvalidity", mailbox->name);
@@ -4090,7 +4093,7 @@ int mailbox_reconstruct(const char *name, int flags)
     if (mailbox->i.highestmodseq == 0) {
 	if (make_changes) {
 	    mailbox_index_dirty(mailbox);
-	    mailbox->i.highestmodseq = 1;
+	    mailbox->i.highestmodseq = mboxname_nextmodseq(mailbox->name, 0);
 	}
 	syslog(LOG_ERR, "%s:  zero highestmodseq", mailbox->name);
     }
