bc92b0b702522e87ea288fe922bb1c3db2412040
Greg Banks <gnb@fastmail.fm>
2011-03-22 09:23:43 +1100

    Add UNTIL to XCONVSORT
    The UNTIL window item changes the reported window such that the position
    designated by ANCHOR/OFFSET or POSITION is at the end of the window
    rather than the start.  UNTIL is orthogonal to OFFSET.
    
    Added an untagged response [POSITION $pos$] to XCONVSORT, which is
    emitted if any results were emitted and indicates the 1-based position
    in the sort order of the reported window.  Removed the internal implicit
    MODSEQ search parameter in the CHANGEDSINCE case, as this was making the
    POSITION reports incorrect.
    
    Also, changed the format of the CHANGED list.  When the CONVERSATIONS
    window item is specified, CHANGED reports conversation ids, but
    otherwise it reports UIDs.
    
    Also refactored the code to use two passes instead of a single pass with
    a lookbehind buffer.  This made the code slightly slower but a lot saner.

--- imap/imapd.c
+++ imap/imapd.c
@@ -5148,9 +5148,6 @@ void cmd_xconvsort(char *tag)
 	goto error;
     }
 
-    if (windowargs->changedsince)
-	searchargs->modseq = windowargs->modseq + 1;
-
     n = index_convsort(imapd_index, sortcrit, searchargs, windowargs);
 
     snprintf(mytime, sizeof(mytime), "%2.3f",
@@ -10266,7 +10263,9 @@ static int parse_windowargs(const char *tag, struct windowargs **wa)
 	    if (c != ')')
 		goto syntax_error;
 	    c = prot_getc(imapd_in);
-	} else
+	} else if (!strcasecmp(arg.s, "UNTIL"))
+	    windowargs.until = 1;
+	else
 	    goto syntax_error;
 
 	if (c == ')')
