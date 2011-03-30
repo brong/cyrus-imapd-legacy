29cfb85f99f644236eee370d8a5f132af475f701
Bron Gondwana <brong@opera.com>
2011-03-30 12:40:00 +0200

    conversations: make "NAME" optional in email addresses

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -2164,7 +2164,6 @@ static int mailbox_update_conversations(struct mailbox *mailbox,
     if (!old && new && !(new->system_flags & (FLAG_EXPUNGED|FLAG_DRAFT))) {
 	if (!mailbox_cacherecord(mailbox, new)) {
 	    char *env = NULL;
-	    char *email = NULL;
 	    char *envtokens[NUMENVTOKENS];
 	    struct address addr = { NULL, NULL, NULL, NULL, NULL, NULL };
 
@@ -2187,7 +2186,6 @@ static int mailbox_update_conversations(struct mailbox *mailbox,
 		delta_attachments++;
 
 	    free(env);
-	    free(email);
 	}
     }
 
