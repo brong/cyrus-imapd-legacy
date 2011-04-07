b315d9c32c3c3f4c9182f372cf75a4b66807a8ad
Bron Gondwana <brong@opera.com>
2011-04-07 21:47:59 +0200

    xconvfetch: don't crash on missing conversation

--- imap/imapd.c
+++ imap/imapd.c
@@ -4763,6 +4763,9 @@ static int xconvfetch_lookup(struct conversations_state *statep,
     r = conversation_load(statep, cid, &conv);
     if (r) return r;
 
+    if (!conv)
+	goto out;
+
     if (!conv->exists)
 	goto out;
 
