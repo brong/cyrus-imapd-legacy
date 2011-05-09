7d6b0bbc18a813f5e51f8348267a6bf7674ef8e1
Greg Banks <gnb@fastmail.fm>
2011-05-09 14:51:51 +1000

    Include MODSEQ in FETCH response from STORE.
    RFC4551 section 3.2 says, in regards to a successful STORE command with
    the UNCHANGEDSINCE modifier:
    
    > An untagged FETCH response MUST be sent, even if the .SILENT suffix is
    > specified, and the response MUST include the MODSEQ message data item.
    
    Cyrus was generating a FETCH response but not including the MODSEQ item.

--- imap/index.c
+++ imap/index.c
@@ -143,7 +143,8 @@ static int index_storeflag(struct index_state *state, uint32_t msgno,
 			   struct storeargs *storeargs);
 static int index_fetchreply(struct index_state *state, uint32_t msgno,
 			    const struct fetchargs *fetchargs);
-static void index_printflags(struct index_state *state, uint32_t msgno, int usinguid);
+static void index_printflags(struct index_state *state, uint32_t msgno,
+			     int usinguid, int printmodseq);
 static char *get_localpart_addr(const char *header);
 static char *get_displayname(const char *header);
 static char *index_extract_subject(const char *subj, size_t len, int *is_refwd);
@@ -626,7 +627,7 @@ void index_select(struct index_state *state, struct index_init *init)
 		continue;
 	    if (im->record.modseq <= init->vanished.modseq)
 		continue;
-	    index_printflags(state, msgno, 1);
+	    index_printflags(state, msgno, 1, 0);
 	}
 	seqset_free(seq);
     }
@@ -671,7 +672,7 @@ int index_check(struct index_state *state, int usinguid, int printuid)
     index_refresh(state);
 
     /* any updates? */
-    index_tellchanges(state, usinguid, printuid);
+    index_tellchanges(state, usinguid, printuid, 0);
 
 #if TOIMSP
     if (state->firstnotseen) {
@@ -921,7 +922,7 @@ int index_fetch(struct index_state *state,
 
     seqset_free(seq);
 
-    index_tellchanges(state, usinguid, usinguid);
+    index_tellchanges(state, usinguid, usinguid, 0);
 
     return r;
 }
@@ -975,7 +976,8 @@ int index_store(struct index_state *state, char *sequence, int usinguid,
 fail:
     seqset_free(seq);
     index_unlock(state);
-    index_tellchanges(state, usinguid, usinguid);
+    index_tellchanges(state, usinguid, usinguid,
+		      (storeargs->unchangedsince != ~0ULL));
 
     return r;
 }
@@ -2649,7 +2651,7 @@ static void index_tellexists(struct index_state *state)
 }
 
 void index_tellchanges(struct index_state *state, int canexpunge,
-		       int printuid)
+		       int printuid, int printmodseq)
 {
     uint32_t msgno;
     struct index_map *im;
@@ -2670,7 +2672,7 @@ void index_tellchanges(struct index_state *state, int canexpunge,
 
 	/* report if it's changed since last told */
 	if (im->record.modseq > im->told_modseq)
-	    index_printflags(state, msgno, printuid);
+	    index_printflags(state, msgno, printuid, printmodseq);
     }
 }
 /*
@@ -2727,7 +2729,8 @@ static void index_fetchflags(struct index_state *state,
 }
 
 static void index_printflags(struct index_state *state,
-			     uint32_t msgno, int usinguid)
+			     uint32_t msgno, int usinguid,
+			     int printmodseq)
 {
     struct index_map *im = &state->map[msgno-1];
 
@@ -2737,7 +2740,7 @@ static void index_printflags(struct index_state *state,
      * untagged FETCH unsolicited responses */
     if (usinguid || state->qresync)
 	prot_printf(state->out, " UID %u", im->record.uid);
-    if (state->qresync)
+    if (printmodseq || state->qresync)
 	prot_printf(state->out, " MODSEQ (" MODSEQ_FMT ")", im->record.modseq);
     prot_printf(state->out, ")\r\n");
 }
