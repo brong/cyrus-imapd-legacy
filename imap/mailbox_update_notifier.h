b2a3abe56340bf449264a35a74f6df781fd193f4
Andrew Wansink <ajwans@fastmail.fm>
2011-06-06 03:39:45 -0400

    PUSHER - squashed up commits
    First cut cyrus -> pusher.pl messaging.
    
    Add Protocol Buffers detection to autoconf.
    
    Redo without old configure stuff
    
    Using internal mboxname_parts method.
    
    Prefer sendto to connect/send.
    
    Don't double log errors from send_push_notification.
    
    Fix some 'const' warnings.
    
    Add IMAP session data to notify message.
    
    When using protobuf it is one of the IMAP libs.
    
    Fix for empty folder notification
    
    Mail deliveries to the root folder (INBOX) were not receiving any
    folder portion when decomposed by mboxname_to_parts, set to "INBOX"
    in this case.
    
    Tweaks after code review.
    
    - rename new files
    - build protocol definitions from .proto description file
    - prefer xmalloc to calloc
    - rename option modseq_notify_socket -> mailbox_update_notifier_socket
    
    Fix guards around mailbox_update_notifier prototypes.
    
    Minor updates after code review
    
    * Modseq field widened to 64 bits in update protocol
    * Initialise unix socket address structure
    * Remove redundant (and now unused) ARRAY_SIZE macro
    
    Thanks gnb!
    
    configure.in fixes
    
    * check for Google Protocol Buffers headers
    * check for protoc-c in path and --with-protobuf directory
    * refactor tests so that all failures set with_gpb to notfound
    * reset LD_FLAGS after libs test and add GPB_LDFLAGS in Makefiles

---
+++ imap/mailbox_update_notifier.h
@@ -0,0 +1,9 @@
+#ifndef MAILBOX_UPDATE_NOTIFIER_H
+#define MAILBOX_UPDATE_NOTIFIER_H
+
+#include <stdint.h>
+#include "mailbox.h"
+
+void send_push_notification(struct mailbox *);
+
+#endif /* MAILBOX_UPDATE_NOTIFIER_H */
