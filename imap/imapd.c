d1aa21b255e5c2b0e61ca895417582219c205aaa
Greg Banks <gnb@fastmail.fm>
2011-08-19 21:03:13 +1000

    Add FOLDER, UIDVALIDITY fetch items
    Merge summary:
    
        split out from "first pass at XCONVFETCH"
        merged in "Report external name for FOLDER fetch item."
        manually tweaked for merge breakage

--- imap/imapd.c
+++ imap/imapd.c
@@ -4267,6 +4267,9 @@ badannotation:
 	    else if (!strcmp(fetchatt.s, "FLAGS")) {
 		fa->fetchitems |= FETCH_FLAGS;
 	    }
+	    else if (!strcmp(fetchatt.s, "FOLDER")) {
+		fa->fetchitems |= FETCH_FOLDER;
+	    }
 	    else goto badatt;
 	    break;
 
@@ -4365,6 +4368,9 @@ badannotation:
 	case 'U':
 	    if (!strcmp(fetchatt.s, "UID")) {
 		fa->fetchitems |= FETCH_UID;
+	    }
+	    else if (!strcmp(fetchatt.s, "UIDVALIDITY")) {
+		fa->fetchitems |= FETCH_UIDVALIDITY;
 	    }
 	    else goto badatt;
 	    break;
@@ -4469,9 +4475,11 @@ badannotation:
 	}
     }
 
-    if (fa->fetchitems & FETCH_ANNOTATION) {
+    if (fa->fetchitems & (FETCH_ANNOTATION|FETCH_FOLDER)) {
 	fa->namespace = &imapd_namespace;
 	fa->userid = imapd_userid;
+    }
+    if (fa->fetchitems & FETCH_ANNOTATION) {
 	fa->isadmin = imapd_userisadmin || imapd_userisproxyadmin;
 	fa->authstate = imapd_authstate;
     }
