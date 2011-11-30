ee64de72adb9d428c27cc533afbd78861cddbd7c
Bron Gondwana <brong@opera.com>
2011-11-30 23:14:04 +0100

    conversation_load can return true but no data, so cope

--- imap/index.c
+++ imap/index.c
@@ -4622,6 +4622,7 @@ static MsgData **index_msgdata_load(struct index_state *state,
 		assert(cstate);
 		if (conversation_load(cstate, im->record.cid, &conv))
 		    continue;
+		if (!conv) conv = conversation_new(cstate);
 		did_conv++;
 	    }
 
