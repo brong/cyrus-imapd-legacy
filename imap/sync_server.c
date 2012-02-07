0ffafbeccf8cc0a24e2874bc266934b3ba1862f9
Bron Gondwana <brong@opera.com>
2012-02-07 20:42:37 +0100

    syslog error_message(r) per gnb code review

--- imap/sync_server.c
+++ imap/sync_server.c
@@ -1428,7 +1428,8 @@ static int do_mailbox(struct dlist *kin)
 	if (!r) mailbox->i.highestmodseq = 0;
     }
     if (r) {
-	syslog(LOG_ERR, "Failed to open mailbox %s to update", mboxname);
+	syslog(LOG_ERR, "Failed to open mailbox %s to update: %s",
+	       mboxname, error_message(r));
 	goto done;
     }
 
@@ -1469,7 +1470,8 @@ static int do_mailbox(struct dlist *kin)
 
 	r = mailbox_get_xconvmodseq(mailbox, &ourxconvmodseq);
 	if (r) {
-	    syslog(LOG_ERR, "Failed to read xconvmodseq for %s", mboxname);
+	    syslog(LOG_ERR, "Failed to read xconvmodseq for %s: %s",
+		   mboxname, error_message(r));
 	    goto done;
 	}
 
@@ -1485,7 +1487,8 @@ static int do_mailbox(struct dlist *kin)
     r = annotatemore_begin();
     if (!r) r = annotate_getdb(mailbox->name, &user_annot_db);
     if (r) {
-	syslog(LOG_ERR, "Failed to open annotations %s to update", mboxname);
+	syslog(LOG_ERR, "Failed to open annotations %s to update: %s",
+	       mboxname, error_message(r));
 	goto done;
     }
 
