45e5981cee5f818321a42524c1c2a8d649c7a1a6
Bron Gondwana <brong@opera.com>
2012-10-02 12:45:17 +0200

    search: remove un-needed msg_orig_count

--- imap/index.c
+++ imap/index.c
@@ -2249,7 +2249,6 @@ struct search_folder {
     unsigned int *msg_list;
     unsigned int msg_count;
     unsigned int alloc;
-    unsigned int msg_orig_count;
     MsgData **msgdata;
 };
 
@@ -2315,7 +2314,6 @@ static void find_uids(struct index_state *state,
 	/* list all messages in the folder */
 	for (msgno = 1; msgno <= state->exists; msgno++)
 	    search_folder_add_uid(sf, msgno);
-	sf->msg_orig_count = sf->msg_count;
 	sf->uidvalidity = state->mailbox->i.uidvalidity;
 	return;
     }
@@ -2327,7 +2325,6 @@ static void find_uids(struct index_state *state,
 	    sf->msg_list[out++] = msgno;
     }
 
-    sf->msg_orig_count = sf->msg_count;
     sf->msg_count = out;
 }
 
@@ -2638,7 +2635,7 @@ out:
     /* free all our temporary data */
     for (fi = 0 ; fi < (unsigned)sr.folders.count ; fi++) {
 	sf = ptrarray_nth(&sr.folders, fi);
-	index_msgdata_free(sf->msgdata, sf->msg_orig_count);
+	index_msgdata_free(sf->msgdata, sf->msg_count);
 	free(sf->mboxname);
 	free(sf->msg_list);
 	free(sf);
