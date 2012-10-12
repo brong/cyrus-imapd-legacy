27f28b7192cca02748e38f224320bf4c1bd005d1
Bron Gondwana <brong@opera.com>
2012-10-12 23:15:26 +0200

    sync_server: log APPEND events

--- imap/sync_server.c
+++ imap/sync_server.c
@@ -1218,6 +1218,7 @@ static int mailbox_compare_update(struct mailbox *mailbox,
     struct sync_annot_list *rannots = NULL;
     int r;
     int i;
+    int has_append = 0;
 
     rrecord.uid = 0;
     for (ki = kr->head; ki; ki = ki->next) {
@@ -1333,8 +1334,13 @@ static int mailbox_compare_update(struct mailbox *mailbox,
 		       mailbox->name, recno);
 		goto out;
 	    }
+
+	    has_append = 1;
 	}
     }
+
+    if (has_append)
+	sync_log_append(mailbox->name);
 
     r = 0;
 
