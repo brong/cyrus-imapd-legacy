377239ca1dd44897380e4359ac38b72cdb88b81e
Greg Banks <gnb@fastmail.fm>
2012-10-19 14:28:21 +1100

    Don't update indexes from XCONVMULTISORT
    as rolling indexing should soon be working again.

--- imap/index.c
+++ imap/index.c
@@ -2162,6 +2162,7 @@ done:
 
 static int update_indexes(struct mailbox *mailbox)
 {
+#if 0
     const char *user = mboxname_to_userid(mailbox->name);
     struct update_index_rock rock;
     int r;
@@ -2174,6 +2175,9 @@ static int update_indexes(struct mailbox *mailbox)
     search_end_update(rock.rx);
 
     return r;
+#else
+    return 0;
+#endif
 }
 
 struct search_folder {
