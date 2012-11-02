5111bf79f30b16286169aca7474d7ae87ab7d690
Bron Gondwana <brong@opera.com>
2012-11-02 08:02:07 +0100

    xconvmultisort: free cachekey

--- imap/index.c
+++ imap/index.c
@@ -3053,7 +3053,7 @@ EXPORTED int index_convmultisort(struct index_state *state,
     char extname[MAX_MAILBOX_BUFFER];
     modseq_t hms;
     struct multisort_result *sortres = NULL;
-    char *cachekey;
+    char *cachekey = NULL;
 
     assert(windowargs);
     assert(!windowargs->changedsince);
@@ -3238,6 +3238,7 @@ out:
     free(sortres->folders);
     free(sortres->msgs);
     free(sortres);
+    free(cachekey);
 
     return r;
 }
