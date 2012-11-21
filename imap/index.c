b202d6de8044e677cd9e425d9273a708b2ea6d80
Greg Banks <gnb@fastmail.fm>
2012-11-21 18:32:54 +1100

    Introduce search_expr
    as a new and better designed replacement for struct searchargs.
    Now can do a SEARCH or SORT on SUBJECT, TO, FROM, CC, BCC, and HEADER
    MESSAGE-ID.
    
    v2:
    renamed from "wip - redesign struct searchargs"
    merged in some hunks from "search: add uid & msgno attributes"

--- imap/index.c
+++ imap/index.c
@@ -125,7 +125,8 @@ static void index_listflags(struct index_state *state);
 static void index_fetchflags(struct index_state *state, uint32_t msgno);
 static int index_search_evaluate(struct index_state *state,
 				 const struct searchargs *searchargs,
-				 uint32_t msgno, struct buf *msgfile);
+				 uint32_t msgno);
+#if 0 /*TODO:gnb*/
 static int index_searchmsg(struct index_record *, const struct buf *msgfile,
 			   const char *substr, comp_pat *pat, int skipheader);
 static int index_searchheader(char *name, char *substr, comp_pat *pat,
@@ -133,6 +134,7 @@ static int index_searchheader(char *name, char *substr, comp_pat *pat,
 			      int size);
 static int index_searchcacheheader(struct index_state *state, uint32_t msgno, char *name, char *substr,
 				   comp_pat *pat);
+#endif
 static int _index_search(unsigned **msgno_list, struct index_state *state,
 			 struct searchargs *searchargs,
 			 modseq_t *highestmodseq);
@@ -1467,7 +1469,7 @@ static int _index_search(unsigned **msgno_list, struct index_state *state,
 	if (!state->want_expunged && (im->record.system_flags & FLAG_EXPUNGED))
 	    continue;
 
-	if (index_search_evaluate(state, searchargs, msgno, NULL)) {
+	if (index_search_evaluate(state, searchargs, msgno)) {
 	    (*msgno_list)[n++] = msgno;
 	    if (highestmodseq && im->record.modseq > *highestmodseq) {
 		*highestmodseq = im->record.modseq;
@@ -1500,7 +1502,7 @@ static int _index_search(unsigned **msgno_list, struct index_state *state,
 	if (!state->want_expunged && (im->record.system_flags & FLAG_EXPUNGED))
 	    continue;
 
-	if (index_search_evaluate(state, searchargs, msgno, NULL)) {
+	if (index_search_evaluate(state, searchargs, msgno)) {
 	    (*msgno_list)[n++] = msgno;
 	    if (highestmodseq && im->record.modseq > *highestmodseq) {
 		*highestmodseq = im->record.modseq;
@@ -1713,6 +1715,7 @@ EXPORTED int index_sort(struct index_state *state, struct sortcrit *sortcrit,
     if (index_check(state, 0, 0))
 	return 0;
 
+#if 0
     /* calculate the user flags */
     index_calcsearchflags(state, searchargs);
 
@@ -1725,6 +1728,8 @@ EXPORTED int index_sort(struct index_state *state, struct sortcrit *sortcrit,
 	    }
 	}
     }
+#endif
+    search_expr_internalise(state->mailbox, searchargs->root);
 
     /* Search for messages based on the given criteria */
     nmsg = _index_search(&msgno_list, state, searchargs,
@@ -2091,7 +2096,7 @@ EXPORTED int index_convsort(struct index_state *state,
 	    continue;
 
 	/* run the search program against all messages */
-	if (!index_search_evaluate(state, searchargs, msg->msgno, NULL))
+	if (!index_search_evaluate(state, searchargs, msg->msgno))
 	    continue;
 
 	/* figure out whether this message is an exemplar */
@@ -2489,7 +2494,7 @@ static struct multisort_result *multisort_run(struct index_state *state,
 		continue;
 
 	    /* run the search program */
-	    if (!index_search_evaluate(state2, searchargs2, msgno, NULL))
+	    if (!index_search_evaluate(state2, searchargs2, msgno))
 		continue;
 
 	    msgs[count++] = msgno;
@@ -3453,7 +3458,7 @@ EXPORTED int index_convupdates(struct index_state *state,
 	int is_changed = 0;
 	int in_search = 0;
 
-	in_search = index_search_evaluate(state, searchargs, msg->msgno, NULL);
+	in_search = index_search_evaluate(state, searchargs, msg->msgno);
 	is_deleted = !!(record->system_flags & FLAG_EXPUNGED);
 	is_new = (record->uid >= windowargs->uidnext);
 	is_changed = (record->modseq > windowargs->modseq);
@@ -5285,6 +5290,7 @@ out:
 }
 
 
+#if 0 /*TODO:gnb*/
 static int _search_searchbuf(char *s, comp_pat *p, struct buf *b)
 {
     if (!b->len)
@@ -5373,6 +5379,7 @@ out:
     strarray_fini(&attribs);
     return r;
 }
+#endif
 
 static int _search_contenttype(const char *str, struct buf *bodystructure)
 {
@@ -5409,14 +5416,12 @@ static int _search_contenttype(const char *str, struct buf *bodystructure)
 
 /*
  * Evaluate a searchargs structure on a msgno
- *
- * Note: msgfile argument must be 0 if msg is not mapped in.
  */
 static int index_search_evaluate(struct index_state *state,
 				 const struct searchargs *searchargs,
-				 uint32_t msgno,
-				 struct buf *msgfile)
+				 uint32_t msgno)
 {
+#if 0
     unsigned i;
     struct strlist *l, *h;
     struct searchsub *s;
@@ -5651,8 +5656,24 @@ zero:
     buf_free(&localmap);
 
     return retval;
+#else
+    struct index_map *im = &state->map[msgno-1];
+    int r;
+    message_t *m;
+
+    xstats_inc(SEARCH_EVALUATE);
+
+    m = message_new_from_index(state->mailbox, &im->record, msgno,
+			       (im->isrecent ? MESSAGE_RECENT : 0) |
+			       (im->isseen ? MESSAGE_SEEN : 0));
+    r = search_expr_evaluate(m, searchargs->root);
+    message_unref(&m);
+
+    return r;
+#endif
 }
 
+#if 0 /*TODO:gnb*/
 /*
  * Search part of a message for a substring.
  */
@@ -5686,8 +5707,10 @@ static int searchmsg_cb(int partno, int charset, int encoding,
 				  charset, encoding, charset_flags);
     }
 }
+#endif
 
 
+#if 0 /*TODO:gnb*/
 static int index_searchmsg(struct index_record *record,
 			   const struct buf *msgfile,
 			   const char *substr,
@@ -5702,7 +5725,9 @@ static int index_searchmsg(struct index_record *record,
     sr.skipheader = skipheader;
     return message_foreach_part(record, msgfile, searchmsg_cb, &sr);
 }
+#endif
 
+#if 0 /*TODO:gnb*/
 /*
  * Search named header of a message for a substring
  */
@@ -5769,6 +5794,7 @@ static int index_searchcacheheader(struct index_state *state, uint32_t msgno,
 
     return charset_search_mimeheader(substr, pat, strchr(buf, ':') + 1, charset_flags);
 }
+#endif
 
 
 struct getsearchtext_rock
