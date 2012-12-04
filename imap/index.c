cb23910ca6e497f6c85936d6eebed17580a7605c
Greg Banks <gnb@fastmail.fm>
2012-12-04 18:22:05 +1100

    Fix memory leak when searching by message-id

--- imap/index.c
+++ imap/index.c
@@ -7849,6 +7849,7 @@ EXPORTED void freesearchargs(struct searchargs *s)
     freestrlist(s->cc);
     freestrlist(s->bcc);
     freestrlist(s->subject);
+    freestrlist(s->messageid);
     freestrlist(s->body);
     freestrlist(s->text);
     freestrlist(s->header_name);
