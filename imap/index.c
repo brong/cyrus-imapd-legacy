45e0f36126397767b90597dcbab2229c1a3e9854
Greg Banks <gnb@fastmail.fm>
2012-12-05 17:32:17 +1100

    search: fix compile warning

--- imap/index.c
+++ imap/index.c
@@ -5595,7 +5595,7 @@ out:
 }
 
 
-#if 0 /*TODO:gnb*/
+#if 0
 static int _search_searchbuf(char *s, comp_pat *p, struct buf *b)
 {
     if (!b->len)
@@ -5603,7 +5603,6 @@ static int _search_searchbuf(char *s, comp_pat *p, struct buf *b)
 
     return charset_searchstring(s, p, b->s, b->len, charset_flags);
 }
-#endif
 
 static int _search_contenttype(const char *str, struct buf *bodystructure)
 {
@@ -5637,6 +5636,7 @@ static int _search_contenttype(const char *str, struct buf *bodystructure)
     buf_free(&xstr);
     return r;
 }
+#endif
 
 /*
  * Evaluate a searchargs structure on a msgno
