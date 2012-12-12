f3c6eff6b63b38ed46b04d1355c3f081f4d96f4b
Greg Banks <gnb@fastmail.fm>
2012-12-13 10:36:20 +1100

    fixup !Move search program parsing to imapparse.c

--- imap/imapd.c
+++ imap/imapd.c
@@ -5353,7 +5353,7 @@ static void cmd_search(char *tag, int usinguid)
     searchargs = new_searchargs(tag, GETSEARCH_CHARSET_KEYWORD|GETSEARCH_RETURN,
 				&imapd_namespace, imapd_userid, imapd_authstate,
 				imapd_userisadmin || imapd_userisproxyadmin);
-    c = get_search_program(imapd_in, searchargs);
+    c = get_search_program(imapd_in, imapd_out, searchargs);
     if (c == EOF) {
 	eatline(imapd_in, ' ');
 	freesearchargs(searchargs);
@@ -5413,7 +5413,7 @@ static void cmd_sort(char *tag, int usinguid)
     searchargs = new_searchargs(tag, GETSEARCH_CHARSET_FIRST,
 				&imapd_namespace, imapd_userid, imapd_authstate,
 				imapd_userisadmin || imapd_userisproxyadmin);
-    c = get_search_program(imapd_in, searchargs);
+    c = get_search_program(imapd_in, imapd_out, searchargs);
     if (c == EOF) goto error;
 
     if (c == '\r') c = prot_getc(imapd_in);
@@ -5521,7 +5521,7 @@ void cmd_xconvsort(char *tag, int updates)
     searchargs = new_searchargs(tag, GETSEARCH_CHARSET_FIRST,
 				&imapd_namespace, imapd_userid, imapd_authstate,
 				imapd_userisadmin || imapd_userisproxyadmin);
-    c = get_search_program(imapd_in, searchargs);
+    c = get_search_program(imapd_in, imapd_out, searchargs);
     if (c == EOF) goto error;
 
     if (c == '\r') c = prot_getc(imapd_in);
@@ -5641,7 +5641,7 @@ static void cmd_xconvmultisort(char *tag)
     searchargs = new_searchargs(tag, GETSEARCH_CHARSET_FIRST,
 				&imapd_namespace, imapd_userid, imapd_authstate,
 				imapd_userisadmin || imapd_userisproxyadmin);
-    c = get_search_program(imapd_in, searchargs);
+    c = get_search_program(imapd_in, imapd_out, searchargs);
     if (c == EOF) goto error;
 
     if (c == '\r') c = prot_getc(imapd_in);
@@ -5719,7 +5719,7 @@ static void cmd_xsnippets(char *tag)
     searchargs = new_searchargs(tag, GETSEARCH_CHARSET_FIRST,
 				&imapd_namespace, imapd_userid, imapd_authstate,
 				imapd_userisadmin || imapd_userisproxyadmin);
-    c = get_search_program(imapd_in, searchargs);
+    c = get_search_program(imapd_in, imapd_out, searchargs);
     if (c == EOF) goto error;
 
     if (c == '\r') c = prot_getc(imapd_in);
@@ -5834,7 +5834,7 @@ static void cmd_thread(char *tag, int usinguid)
     searchargs = new_searchargs(tag, GETSEARCH_CHARSET_FIRST,
 				&imapd_namespace, imapd_userid, imapd_authstate,
 				imapd_userisadmin || imapd_userisproxyadmin);
-    c = get_search_program(imapd_in, searchargs);
+    c = get_search_program(imapd_in, imapd_out, searchargs);
     if (c == EOF) {
 	eatline(imapd_in, ' ');
 	freesearchargs(searchargs);
