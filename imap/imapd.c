c5c23383c2cb7b5f4ece30c460b6b08e7bf29e59
Greg Banks <gnb@fastmail.fm>
2012-12-14 15:54:58 +1100

    Implement RFC6203 Fuzzy Search
    Protocol visible changes:
    
    - add the SEARCH=FUZZY post-auth capability
    - add the RELEVANCY sort item - which does nothing
    - add the RELEVANCY search return item - which just emits 100 for everything
    - add the FUZZY search criterion which makes the immediately following
      search criterion fuzzy if possible, or is silently ignored otherwise
    
    e.g.
    
       FUZZY SUBJECT "foo"
    
    does a fuzzy match on message subject against "foo"
    
       FUZZY (FOLDER "inbox" SUBJECT "bar" LARGER 123)
    
    does a fuzzy match on message subject against "foo", and precise
    matches on folder and message size.
    
    Search backend changes:
    
    - add a new SEOP_FUZZYMATCH
    - consider only FUZZYMATCH nodes as indexable when splitting queries

--- imap/imapd.c
+++ imap/imapd.c
@@ -292,6 +292,7 @@ static struct capa_struct base_capabilities[] = {
     { "CATENATE",              2 },
     { "CONDSTORE",             2 },
     { "ESEARCH",               2 },
+    { "SEARCH=FUZZY",	       2 }, /* RFC 6203 */
     { "SORT",                  2 },
     { "SORT=MODSEQ",           2 },
     { "SORT=DISPLAY",          2 },
@@ -10969,6 +10970,8 @@ static int getsortcriteria(char *tag, struct sortcrit **sortcrit)
 	}
 	else if (!strcmp(criteria.s, "folder"))
 	    (*sortcrit)[n].key = SORT_FOLDER;
+	else if (!strcmp(criteria.s, "relevancy"))
+	    (*sortcrit)[n].key = SORT_RELEVANCY;
 	else {
 	    prot_printf(imapd_out, "%s BAD Invalid Sort criterion %s\r\n",
 			tag, criteria.s);
