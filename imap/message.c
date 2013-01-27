b5e28f2e04ba2740d5019c477e1cedb40f9b2db8
Bron Gondwana <brong@opera.com>
2013-01-27 22:31:04 +1100

    [IRIS-2513] fix ctl_conversationsdb -b without breaking sync
    Also fixed -z to actually clear records to allow easier testing of -b.

--- imap/message.c
+++ imap/message.c
@@ -2828,8 +2828,12 @@ out:
 
     if (r)
 	conversation_free(conv);
-    else
+    else if (convp)
 	*convp = conv;
+    else {
+	r = conversation_save(state, record->cid, conv);
+	conversation_free(conv);
+    }
 
     return r;
 }
