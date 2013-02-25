57480c6bf2ba2bec6a6ba940e996a8a4822dc944
Bron Gondwana <brong@fastmail.fm>
2013-02-25 15:18:20 +1100

    index: always copy modseq into msgdata
    Fixes missing convupdates response in non-conversations mode

--- imap/index.c
+++ imap/index.c
@@ -4744,6 +4744,9 @@ MsgData **index_msgdata_load(struct index_state *state,
 	if (found_anchor && record.uid == anchor)
 	    *found_anchor = 1;
 
+	/* useful for convupdates */
+	cur->modseq = record.modseq;
+
 	did_cache = did_env = did_conv = 0;
 	tmpenv = NULL;
 	conv = NULL; /* XXX: use a hash to avoid re-reading? */
@@ -4806,7 +4809,7 @@ MsgData **index_msgdata_load(struct index_state *state,
 		cur->from = get_localpart_addr(cacheitem_base(&record, CACHE_FROM));
 		break;
 	    case SORT_MODSEQ:
-		cur->modseq = record.modseq;
+		/* already copied above */
 		break;
 	    case SORT_SIZE:
 		cur->size = record.size;
