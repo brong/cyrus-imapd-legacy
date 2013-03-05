e158ab032cd9408f11cc142bb6173d031ce3926d
Bron Gondwana <brong@fastmail.fm>
2013-03-05 13:33:54 +1100

    sequence: exports and const

--- imap/sequence.h
+++ imap/sequence.h
@@ -76,7 +76,7 @@ void seqset_add(struct seqset *seq, unsigned num, int ismember);
 extern struct seqset *seqset_parse(const char *sequence,
 				   struct seqset *set,
 				   unsigned maxval);
-extern void seqset_join(struct seqset *a, struct seqset *b);
+extern void seqset_join(struct seqset *a, const struct seqset *b);
 extern void seqset_append(struct seqset **l, char *sequence, unsigned maxval);
 extern int seqset_ismember(struct seqset *set, unsigned num);
 extern unsigned seqset_getnext(struct seqset *set);
@@ -84,6 +84,6 @@ extern unsigned seqset_first(const struct seqset *set);
 extern unsigned seqset_last(const struct seqset *set);
 extern char *seqset_cstring(const struct seqset *set);
 extern struct seqset *seqset_dup(const struct seqset *);
-extern void seqset_free(struct seqset *l);
+extern void seqset_free(struct seqset *set);
 
 #endif /* SEQUENCE_H */
