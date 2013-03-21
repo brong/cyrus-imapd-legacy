b84e7015084f7388851ea202736cc407c6af7ca6
Bron Gondwana <brong@opera.com>
2013-03-22 10:53:23 +1100

    index_sort rebase fixup

--- imap/index.c
+++ imap/index.c
@@ -1872,7 +1872,7 @@ out:
  */
 EXPORTED int index_sort(struct index_state *state,
 	       const struct sortcrit *sortcrit,
-	       struct searchargs *searchargs, int usinguid)
+	       const struct searchargs *searchargs, int usinguid)
 {
     int i;
     int nmsg = 0;
