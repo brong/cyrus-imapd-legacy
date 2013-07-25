a7d6f8f2a967ea03345ec8d66ac046bd4dbbd0ba
Bron Gondwana <brong@fastmail.fm>
2013-07-25 10:45:45 +1000

    search: always index the target of a rename

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -1557,6 +1557,8 @@ EXPORTED int mboxlist_renamemailbox(const char *oldname, const char *newname,
 
 	/* log the rename */
 	sync_log_mailbox_double(oldname, newname);
+	/* and log an append so that squatter indexes it */
+	sync_log_append(newname);
     }
 
     /* free memory */
