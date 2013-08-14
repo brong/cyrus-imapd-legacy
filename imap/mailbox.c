3128bd123f8c9af5bc28d0c22e0e29871410fd8d
Bron Gondwana <brong@opera.com>
2013-08-14 15:21:22 +1000

    auditlog archiving events

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -3948,6 +3948,10 @@ EXPORTED void mailbox_archive(struct mailbox *mailbox,
 	    if (mailbox_cacherecord(mailbox, &record))
 		continue;
 	    record.system_flags |= FLAG_ARCHIVED;
+	    if (config_auditlog)
+		syslog(LOG_NOTICE, "auditlog: archive sessionid=<%s> mailbox=<%s> uniqueid=<%s> uid=<%u> guid=<%s> cid=<%s>",
+		       session_id(), mailbox->name, mailbox->uniqueid, record.uid,
+		       message_guid_encode(&record.guid), conversation_id_encode(record.cid));
 	}
 	else {
 	    if (!(record.system_flags & FLAG_ARCHIVED))
@@ -3958,6 +3962,10 @@ EXPORTED void mailbox_archive(struct mailbox *mailbox,
 	    if (mailbox_cacherecord(mailbox, &record))
 		continue;
 	    record.system_flags &= ~FLAG_ARCHIVED;
+	    if (config_auditlog)
+		syslog(LOG_NOTICE, "auditlog: unarchive sessionid=<%s> mailbox=<%s> uniqueid=<%s> uid=<%u> guid=<%s> cid=<%s>",
+		       session_id(), mailbox->name, mailbox->uniqueid, record.uid,
+		       message_guid_encode(&record.guid), conversation_id_encode(record.cid));
 	}
 
 	/* got a file to copy! */
