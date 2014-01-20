f27c48951e4711b889c6509b94e30c72a5888829
Bron Gondwana <brong@fastmail.fm>
2014-01-20 16:45:16 +1100

    mboxlist: don't forget the ACL!

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -375,6 +375,7 @@ EXPORTED int mboxlist_parse_entry(mbentry_t **mbentryptr,
 	memset(&rock, 0, sizeof(struct parseentry_rock));
 	rock.mbentry = mbentry;
 	r = dlist_parsesax(data, datalen, 0, parseentry_cb, &rock);
+	if (!r) mbentry->acl = buf_release(&rock.aclbuf);
 	goto done;
     }
 
