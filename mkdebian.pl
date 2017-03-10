883bb841f2a9e4b7a79eebe40a8d122cb13e3cd5
Bron Gondwana <brong@fastmail.fm>
2017-03-10 15:03:40 +1100

    v12

--- mkdebian.pl
+++ mkdebian.pl
@@ -11,7 +11,7 @@ chomp($branch);
 
 my $date = `date -R`;
 
-my $CYRUSLIBS = "cyruslibs-fastmail-v11";
+my $CYRUSLIBS = "cyruslibs-fastmail-v12";
 
 my $basename = "cyrus-$branch";
 my $basedir = $branch eq 'fastmail' ? 'usr/cyrus' : "usr/$basename";
