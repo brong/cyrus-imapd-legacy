b780d590cd83f8830a1b9b4fc30ebb340945fe54
Bron Gondwana <brong@fastmailteam.com>
2022-09-14 10:21:42 +1000

    update all the bits

--- contrib/docker-test-server/entrypoint.sh
+++ contrib/docker-test-server/entrypoint.sh
@@ -2,4 +2,4 @@
 
 service rsyslog start
 /usr/cyrus/bin/master -p /var/run/cyrus/master.pid -d -L /var/run/cyrus/log
-/srv/cyrus-imapd.git/contrib/docker-test-server/webserver.pl prefork -l http://*:80
+/srv/cyrus-imapd.git/contrib/docker-test-server/webserver.pl prefork -l http://*:8001
